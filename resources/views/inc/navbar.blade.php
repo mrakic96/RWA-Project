3279574075e208221db79885959350bb7d862f17
mrakic96 <matej.rakic96@gmail.com>
2019-12-14 15:15:28 +0100

    Added 'Dashboard' item in dropdown list

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -44,6 +44,14 @@
                               </a>
 
                               <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <!-- Dashboard Link for admins and moderators -->
+                                @is(['admin', 'moderator'])
+
+                                  <a class="dropdown-item" href="/dashboard">
+                                      {{ __('Dashboard') }}
+                                  </a>
+                                    
+                                @endis
                                   <a class="dropdown-item" href="{{ route('logout') }}"
                                      onclick="event.preventDefault();
                                                    document.getElementById('logout-form').submit();">
