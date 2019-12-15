32b609f1e81787649b914ce7663b7ecdc9c69ddb
mrakic96 <matej.rakic96@gmail.com>
2019-12-15 19:22:11 +0100

    Change language in /password views and in verify view

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,20 +5,20 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Potvrdite Vaš e-mail') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Novi link za potvrdu Vam je poslan na e-mail.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Prije nego što nastavite provjerite Vaš email.') }}
+                    {{ __('Ukoliko niste primili ovaj e-mail') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('pritisnite ovdje da zatražite novi') }}</button>.
                     </form>
                 </div>
             </div>
